eeaab0d47497425ec6030f4101af0737e3b75b04
sezerulasli <sezer_ulasli@hotmail.com>
2023-12-29 01:53:42 +0300

    some changes and updates.

---
+++ database.js
@@ -0,0 +1,65 @@
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
+let instance = null;
+
+const con = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+con.connect((error) => {
+    if (error) throw error;
+    console.log("Connected.");
+});
+
+class Db {
+    static getDbInst() {
+        return instance ? instance : new Db();
+    }
+    async getData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                con.query("SELECT * FROM cars", (err, result) => {
+                    if (err) reject(new Error(err.mesage));
+                    resolve(result);
+                });
+            });
+            return response;
+        } catch (error) {
+            throw error;
+        };
+    };
+
+    async searchData(car_brand, car_model, car_year) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                con.query("SELECT * FROM cars WHERE car_brand = ? AND car_model = ? AND car_year = ?", [car_brand, car_model, car_year], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result);
+                });
+            });
+            return response;
+        } catch (error) {
+            throw error;
+        };
+    };
+
+    async addData(car_brand, car_model, car_year) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                con.query("INSERT INTO cars (car_brand, car_model, car_year) VALUES (?,?, ?)", [car_brand, car_model, car_year], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result);
+                });
+            });
+            return response;
+        } catch (error) {
+            throw error;
+        };
+    };
+}
+
+module.exports = Db;
---
+++ views/script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/get")
+        .then(res => res.json())
+        .then(data => loadIndexTable(data["data"]));
+
+
+});
+
+const searchBtn = document.querySelector("#search");
+
+searchBtn.onclick = function() {
+    const searchBrand = document.querySelector("#brandId").value;
+    const searchModel = document.querySelector("#modelId").value;
+    const searchYear = document.querySelector("#yearId").value;
+    fetch("http://localhost:3000/search/" + searchBrand + "/" + searchModel + "/" + searchYear)
+        .then(response => response.json())
+        .then(data => loadIndexTable(data["data"]));
+
+
+}
+const addBtn = document.querySelector("#add");
+
+addBtn.onclick = function() {
+    const addBrand = document.querySelector("#brandId").value;
+    const addModel = document.querySelector("#modelId").value;
+    const addYear = document.querySelector("#yearId").value;
+    fetch("http://localhost:3000/create/", {
+            method: "post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            //make sure to serialize your JSON body
+            body: JSON.stringify({
+                car_brand: addBrand,
+                car_model: addModel,
+                car_year: addYear
+            })
+        })
+        .then(response => response.json())
+
+}
+
+function loadIndexTable(data) {
+    const table = document.querySelector("#table");
+    console.log(data);
+    const arr = Array.from(data);
+    let tableDetails = "";
+    tableDetails += "<thead>";
+    tableDetails += "<tr>";
+    tableDetails += "<th scope='col'>ID</th>";
+    tableDetails += "<th scope='col'>BRAND</th>";
+    tableDetails += "<th scope='col'>MODEL</th>";
+    tableDetails += "<th scope='col'>YEAR</th>";
+    tableDetails += "</tr>";
+    tableDetails += "</thead>";
+    arr.forEach(({ car_id, car_brand, car_model, car_year }) => {
+
+        tableDetails += "<tbody>";
+        tableDetails += `<td scope='col'>${car_id}</td>`;
+        tableDetails += `<td scope='col'>${car_brand}</td>`;
+        tableDetails += `<td scope='col'>${car_model}</td>`;
+        tableDetails += `<td scope='col'>${car_year}</td>`;
+        tableDetails += "</tbody>";
+    })
+
+    table.innerHTML = tableDetails;
+}
